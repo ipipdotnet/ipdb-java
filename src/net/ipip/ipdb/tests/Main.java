c64f5a0efacc579e9a8baa751b5754798c8f54ce
小宝 <ilsanboo@gmail.com>
2018-11-02 19:29:56 +0800

    update

--- src/net/ipip/ipdb/tests/Main.java
+++ src/net/ipip/ipdb/tests/Main.java
@@ -9,11 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        testCityV4();
+
         testBaseStation();
 
         testFree();
         testIDC();
         testDistrict();
+        testCity();
     }
 
     public static void testBaseStation() {
@@ -55,12 +58,13 @@ public class Main {
 
             System.out.println(Arrays.toString(db.find("1.12.13.1", "CN")));
 
-            DistrictInfo info = db.findInfo("8.8.8.8", "CN");
+            DistrictInfo info = db.findInfo("1.12.13.1", "CN");
             if (info != null) {
+                System.out.println(info);
                 System.out.println(info.getCountryName());
             }
 
-            Map<String, String> m = db.findMap("114.114.114.114", "CN");
+            Map<String, String> m = db.findMap("1.12.13.1", "CN");
 
             System.out.println(m);
 
@@ -88,6 +92,23 @@ public class Main {
         }
     }
 
+    public static void testCityV4() {
+        try {
+            City db = new City("C:/work/ipdb/city.ipv4.ipdb");
+
+            System.out.println(Arrays.toString(db.find("1.1.1.1", "CN")));
+
+            CityInfo info = db.findInfo("118.28.1.1", "CN");
+
+            System.out.println(info);
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void testCity() {
         try {
             City db = new City("C:/work/ipdb/city.ipv6.ipdb");
@@ -96,7 +117,7 @@ public class Main {
 
             CityInfo info = db.findInfo("2001:250:201::", "CN");
 
-            System.out.println(info.getCountryName());
+            System.out.println(info);
 
             Map<String, String> m = db.findMap("2001:250:220::", "CN");
 
