7e5d165a660402130422b02de2f7aadc82741b15
小宝 <ilsanboo@gmail.com>
2018-12-13 17:12:16 +0800

    remove upgrade to maven

--- src/net/ipip/ipdb/IPFormatException.java
+++
@@ -1,9 +0,0 @@
-package net.ipip.ipdb;
-
-
-public class IPFormatException extends Exception {
-
-    public IPFormatException(String name) {
-        super(name);
-    }
-}
--- src/net/ipip/ipdb/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package net.ipip.ipdb;
-
-
-public class NotFoundException extends Exception {
-
-    public NotFoundException(String name) {
-        super(name);
-    }
-}
